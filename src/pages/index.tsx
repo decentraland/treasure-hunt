1ef5919d37d6fc673a29d4e9cda54f5c114d442b
Fede <i@2fd.me>
2020-02-26 17:08:08 -0300

    update module

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useRef } from "react"
-// import { transform } from "framer-motion"
+import React, { useState, useEffect } from "react"
 import { useFormatMessage, useFormatListOfMessage } from "~/utils/intl"
+import { Eth } from 'web3x/eth'
 
 // import { Link } from "gatsby"
 import {
@@ -39,6 +39,9 @@ import Bold from "~/components/Section/Bold"
 
 import "./index.css"
 import Link from "~/components/Section/Link"
+import { stat } from "fs"
+import { Address } from "web3x/address"
+import { PartialTransactionRequest } from "web3x/formatters"
 
 const logo = require("~/images/logo.svg")
 const closedChest = require("~/images/closed-chest.png")
@@ -46,6 +49,7 @@ const almostOpenedChest = require("~/images/almost-opened-chest.png")
 const openedChest = require("~/images/opened-chest.png")
 
 const images: { [key: string]: string | undefined } = {
+  'Aeyon Death Run Project': require("../images/Death Run.jpg"),
   'After the Flood: Two Towers': require("../images/After the Flood.jpg"),
   'Asian Plaza': require("../images/Asian Plaza.jpg"),
   'Back To The Past': require("../images/Back To The Past.jpg"),
@@ -54,12 +58,12 @@ const images: { [key: string]: string | undefined } = {
   'Blockrunner': require("../images/BlockRunner.jpg"),
   'Chateau satoshi': require("../images/Chateau satoshi.jpg"),
   'Coin Rush': require("../images/CoinRush.jpg"),
-  'Aeyon Death Run Project': require("../images/Death Run.jpg"),
   'Dinosaur hunt': require("../images/Dinosaur hunt Can you find them all_.jpg"),
   'Dragon race': require("../images/Dragon Race.jpg"),
   'Eden Project': require("../images/Eden Project_.jpg"),
   'Enchannelled Wood': require("../images/Enchannelled Wood.jpg"),
   'Fantasy World of Endless Time': require("../images/Fantasy World of Endless Time.jpg"),
+  'Festivaland District': require("../images/Festival Land.png"),
   'Forest Plaza': require("../images/Forest Plaza.jpg"),
   'Fruit Catcher': require("../images/Fruit Catcher.jpg"),
   'Gamer Plaza': require("../images/Gamer Plaza.jpg"),
@@ -85,8 +89,8 @@ const images: { [key: string]: string | undefined } = {
   'Techno Oasis': require("../images/Techno Oasis_.jpg"),
   'The Cat Came Back': require("../images/The Cat Came Back.jpg"),
   'The Farm': require("../images/The Farm.jpg"),
-  'The Walk': require("../images/The Walk.jpg"),
   'The floor is lava': require("../images/The floor is lava.jpg"),
+  'The Walk': require("../images/The Walk.jpg"),
   'Toki Land': require("../images/Toki Land.jpg"),
   'Tomb Chaser': require("../images/Tomb Chaser.jpg"),
   'Vegas City District': require("../images/Vegas City District.jpg"),
@@ -97,54 +101,54 @@ const images: { [key: string]: string | undefined } = {
 
 
 const scenes = [
-  { "name": "The Farm", "x": -3, "y": -33 },
-  { "name": "Gamer Plaza", "x": 72, "y": -9 },
-  { "name": "Serenity Casino", "x": -55, "y": 143 },
-  { "name": "MoonShot", "x": 58, "y": 2 },
-  { "name": "Blockrunner", "x": 61, "y": -27 },
-  { "name": "Fantasy World of Endless Time", "x": -49, "y": -41 },
-  { "name": "The Cat Came Back", "x": 36, "y": 46 },
-  { "name": "Infinity Engine", "x": -71, "y": -38 },
-  { "name": "Midas Car Park", "x": -129, "y": -141 },
-  { "name": "Space Office", "x": 52, "y": 2 },
-  { "name": "Koko Jones", "x": -39, "y": 58 },
-  { "name": "Fruit Catcher", "x": 59, "y": 133 },
-  { "name": "Vertical garden", "x": 57, "y": 8 },
-  { "name": "Present Predicament", "x": -40, "y": -49 },
-  { "name": "Dinosaur hunt", "x": -12, "y": -39 },
-  { "name": "Forest Plaza", "x": -9, "y": 73 },
-  { "name": "The floor is lava", "x": 87, "y": 18 },
-  { "name": "Battleracers", "x": 67, "y": -21 },
-  { "name": "Chateau satoshi", "x": -75, "y": 73 },
-  { "name": "Wonder World", "x": -15, "y": -22 },
-  { "name": "Enchannelled Wood", "x": -34, "y": -37 },
-  { "name": "Matic", "x": 52, "y": 16 },
-  { "name": "Medieval Plaza", "x": -71, "y": -71 },
-  { "name": "Salmonomicon", "x": -55, "y": 1 },
-  { "name": "Vegas City District", "x": -25, "y": 103 },
-  { "name": "The Walk", "x": 52, "y": 10 },
-  { "name": "Tomb Chaser", "x": 12, "y": 46 },
-  { "name": "Merlin's Keep", "x": -5, "y": -16 },
   { "name": "Aeyon Death Run Project", "x": 105, "y": -21 },
-  { "name": "Quest of an Alcoholic Scarecrow_", "x": -11, "y": -30 },
-  { "name": "Back To The Past", "x": -49, "y": -49 },
-  { "name": "Maze Race", "x": 113, "y": -7 },
-  { "name": "Asian Plaza", "x": 52, "y": -71 },
-  { "name": "Dragon race", "x": -43, "y": 53 },
-  { "name": "Space Odyssey", "x": 63, "y": 2 },
-  { "name": "Mystery Castle", "x": -134, "y": -121 },
-  { "name": "Toki Land", "x": 28, "y": 45 },
-  { "name": "Neo DCL City", "x": 137, "y": 34 },
-  { "name": "Gingerbread breakdown ", "x": -43, "y": 57 },
-  { "name": "Museum District ", "x": 16, "y": 83 },
-  { "name": "Barter Town", "x": 60, "y": 115 },
-  { "name": "Eden Project", "x": -40, "y": 33 },
-  { "name": "Festivaland District", "x": -75, "y": 65 },
   { "name": "After the Flood: Two Towers", "x": -48, "y": 58 },
-  { "name": "Techno Oasis", "x": -35, "y": -41 },
-  { "name": "SoYou", "x": 24, "y": -124 },
+  { "name": "Asian Plaza", "x": 52, "y": -71 },
+  { "name": "Back To The Past", "x": -49, "y": -49 },
+  { "name": "Barter Town", "x": 60, "y": 115 },
+  { "name": "Battleracers", "x": 67, "y": -21 },
+  { "name": "Blockrunner", "x": 61, "y": -27 },
+  { "name": "Chateau satoshi", "x": -75, "y": 73 },
   { "name": "Coin Rush", "x": -148, "y": -35 },
+  { "name": "Dinosaur hunt", "x": -12, "y": -39 },
+  { "name": "Dragon race", "x": -43, "y": 53 },
+  { "name": "Eden Project", "x": -40, "y": 33 },
+  { "name": "Enchannelled Wood", "x": -34, "y": -37 },
+  { "name": "Fantasy World of Endless Time", "x": -49, "y": -41 },
+  { "name": "Festivaland District", "x": -65, "y": 77 },
+  { "name": "Forest Plaza", "x": -9, "y": 73 },
+  { "name": "Fruit Catcher", "x": 59, "y": 133 },
+  { "name": "Gamer Plaza", "x": 72, "y": -9 },
+  { "name": "Gingerbread breakdown ", "x": -43, "y": 57 },
+  { "name": "Infinity Engine", "x": -71, "y": -38 },
+  { "name": "Koko Jones", "x": -39, "y": 58 },
+  { "name": "Matic", "x": 52, "y": 16 },
+  { "name": "Maze Race", "x": 113, "y": -7 },
+  { "name": "Medieval Plaza", "x": -71, "y": -71 },
+  { "name": "Merlin's Keep", "x": -5, "y": -16 },
+  { "name": "Midas Car Park", "x": -129, "y": -141 },
+  { "name": "MoonShot", "x": 58, "y": 2 },
+  { "name": "Museum District ", "x": 16, "y": 83 },
+  { "name": "Mystery Castle", "x": -134, "y": -121 },
+  { "name": "Neo DCL City", "x": 137, "y": 34 },
+  { "name": "Present Predicament", "x": -40, "y": -49 },
+  { "name": "Quest of an Alcoholic Scarecrow_", "x": -11, "y": -30 },
+  { "name": "Salmonomicon", "x": -55, "y": 1 },
+  { "name": "Serenity Casino", "x": -55, "y": 143 },
+  { "name": "SoYou", "x": 24, "y": -124 },
+  { "name": "Space Odyssey", "x": 63, "y": 2 },
+  { "name": "Space Office", "x": 52, "y": 2 },
+  { "name": "Techno Oasis", "x": -35, "y": -41 },
+  { "name": "The Cat Came Back", "x": 36, "y": 46 },
+  { "name": "The Farm", "x": -3, "y": -33 },
+  { "name": "The floor is lava", "x": 87, "y": 18 },
+  { "name": "The Walk", "x": 52, "y": 10 },
+  { "name": "Toki Land", "x": 28, "y": 45 },
+  { "name": "Tomb Chaser", "x": 12, "y": 46 },
+  { "name": "Vegas City District", "x": -25, "y": 103 },
+  { "name": "Vertical garden", "x": 57, "y": 8 },
   { "name": "Volcano", "x": -109, "y": -89 },
+  { "name": "Wonder World", "x": -15, "y": -22 },
 ]
 
 export const sceneNames = new Set(scenes.map(s => s.name))
@@ -154,6 +158,9 @@ export type Reward = {
   "user": string,
   "createdAt": string,
   "name": string,
+  contract: string,
+  transaction: string,
+  "status": 'READY' | 'SUCCESS',
   "rewardItems": {
     id: string,
     type: string | null
@@ -170,17 +177,31 @@ export type Reward = {
 
 export type IndexPageState = {
   loading?: boolean
-  filter?: 'ALL' | 'COMPLETED' | 'PENDING' | 'CLAIMED' | 'UNCLAIMED'
+  filter?: 'ALL' | 'COMPLETED' | 'INCOMPLETE' | 'CLAIMED' | 'UNCLAIMED'
+  address?: string,
   data?: Map<string, Reward>
   error?: Error
 }
+
+let ETH: Eth | null;
+
+async function eth() {
+  if (ETH === undefined) {
+    const ethereum = (window as any).ethereum
+    await ethereum.enable()
+    ETH = Eth.fromCurrentProvider() || null
+  }
+
+  return ETH
+}
+
 
 const RandomWallet = () => '0x' + Array.from(new Array(40), () => Math.floor(Math.random() * 16).toString(16)).join('')
 
 export default function IndexPage(props: any) {
   // return null
   const l = useFormatMessage()
-  const ll = useFormatListOfMessage()
+  // const ll = useFormatListOfMessage()
   // const language: keyof typeof Interests = props?.pageContext?.intl?.language || 'en'
   // const [currentEmail, setCurrentEmail] = useState("")
   // const [subscribeStatus, setSubscribeStatus] = useState<SubscribeStatus>(
@@ -190,17 +211,17 @@ export default function IndexPage(props: any) {
   const [state, setState] = useState<IndexPageState>({})
 
   async function loadAddressData(address?: string) {
-    const setNewState = (newState: Partial<IndexPageState>) => window.location.hash.slice(2) === address && setState({ ...state, ...newState })
+    const setNewState = (newState: Partial<IndexPageState>) => window.location.hash.slice(2) === address && setState((current) => ({ ...current, ...newState }))
 
     setNewState({ loading: true })
 
     try {
       const res = await fetch(`https://giveaways.decentraland.org/api/rewards/${address}`)
-      const json = await res.json() as { statusCode: number, message: string, data: Reward[] }
+      const json = await res.json() as { statusCode: number, message: string, address: string, data: Reward[] }
 
       if (res.status === 200) {
         const data = new Map((json.data || []).filter(data => sceneNames.has(data.name)).map(data => [data.name, data]))
-        setNewState({ loading: false, data })
+        setNewState({ loading: false, data, address: json.address })
         let element = document.querySelector('form') as HTMLElement
         let top = element.offsetTop || 0
         while (element.parentElement) {
@@ -244,6 +265,46 @@ export default function IndexPage(props: any) {
 
   }, [])
 
+  async function handleConnectWallet() {
+    setState((current) => ({ ...current, loading: true }))
+    try {
+      const ethereum = await eth()
+      if (ethereum) {
+        const accounts = await ethereum.getAccounts()
+        if (accounts.length) {
+          window.location.hash = '#/' + accounts[0].toString()
+        }
+      }
+    } catch (err) {
+      console.error(err)
+      setState((current) => ({ ...current, loading: false }))
+    }
+  }
+
+  async function handleClaim(contract: string, data: string) {
+    const ethereum = await eth()
+    const address = state.address as string
+    const from = Address.fromString(address)
+    const to = Address.fromString(contract)
+    if (ethereum && state.address) {
+      try {
+        const gasPrice = await ethereum.getGasPrice()
+        const transaction: PartialTransactionRequest = {
+          from,
+          to,
+          value: 0,
+          gas: 21000,
+          gasPrice: 6 * Number(gasPrice),
+          data: Buffer.from(data)
+        }
+        console.log(transaction)
+        await ethereum.sendTransaction(transaction)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+
   function handleAddressChange(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
     const input: HTMLInputElement = Array.from((event.target as any).elements).find((e: any) => e.name === 'address') as any
@@ -259,9 +320,9 @@ export default function IndexPage(props: any) {
   const sceneList = state.data && scenes
     .filter(scene => {
       const reward = state.data?.get(scene.name)
-      const hasPendingRewardItems = !(reward?.rewardItems || []).some(item => item.completed)
+      const hasPendingRewardItems = reward && reward.status !== 'SUCCESS'
       switch (state.filter) {
-        case 'PENDING': return !reward
+        case 'INCOMPLETE': return !reward
         case 'COMPLETED': return !!reward
         case 'CLAIMED': return !!reward && !hasPendingRewardItems
         case 'UNCLAIMED': return !!reward && hasPendingRewardItems
@@ -286,17 +347,24 @@ export default function IndexPage(props: any) {
           <SubTitle>{l("hunt.main.description")}</SubTitle>
         </Hero.Header>
         <Hero.Actions className="hero-actions hero-subscribe">
-          <form onSubmit={handleAddressChange}>
-            <Input
+          {/* <form onSubmit={handleAddressChange}> */}
+          {/* <Input
               name="address"
               placeholder={RandomWallet()}
-            />
-            <Button
-              primary
-            >
-              {l(`hunt.main.cta`)}
-            </Button>
-          </form>
+          /> */}
+          {!state.address && <Button
+            primary
+            loading={state.loading}
+            onClick={handleConnectWallet}
+          >
+            {l(`hunt.main.cta`)}
+          </Button>}
+          {state.address && <Button
+            primary
+          >
+            wallet: {state.address}
+          </Button>}
+          {/* </form> */}
         </Hero.Actions>
         {/* <HeroContent /> */}
       </Hero>
@@ -314,7 +382,7 @@ export default function IndexPage(props: any) {
                   <span style={{ marginRight: '.5rem' }}>FILTER:</span>
                   <Dropdown onChange={handleFilterChange} defaultValue={state.filter || 'ALL'} pointing="top right" options={[
                     { text: 'ALL SCENES', value: 'ALL' },
-                    { text: 'PENDING SCENES', value: 'PENDING' },
+                    { text: 'INCOMPLETE SCENES', value: 'INCOMPLETE' },
                     { text: 'COMPLETED SCENES', value: 'COMPLETED' },
                     { text: 'CLAIMED REWARDS', value: 'CLAIMED' },
                     { text: 'UNCLAIMED REWARDS', value: 'UNCLAIMED' },
@@ -329,9 +397,9 @@ export default function IndexPage(props: any) {
             {sceneList && sceneList.map((scene) => {
               const reward = state.data?.get(scene.name)
               const hasRewards = !!reward
-              const hasRewardItems = reward?.rewardItems && reward.rewardItems.length > 0
               const rewardsItems = (reward?.rewardItems || [])
-              const hasPendingRewardItems = !rewardsItems.some(item => item.completed)
+              const hasRewardItems = rewardsItems.length > 0
+              const hasPendingRewardItems = reward && reward.status !== 'SUCCESS'
               const image = images[scene.name as any] as any || `https://api.decentraland.org/v1/parcels/${scene.x}/${scene.y}/map.png`
               return <Grid.Row key={scene.name} verticalAlign="top">
                 <Grid.Column mobile={5}>
@@ -343,12 +411,12 @@ export default function IndexPage(props: any) {
                 <Grid.Column mobile={8} style={{ height: '100%' }}>
                   <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', height: '100%' }}>
                     <div>
-                      <SubTitle>{scene.name} <Link href={`https://play.decentraland.org/?position=${scene.x}%2C${scene.y}`}>({scene.x},{scene.y})</Link></SubTitle>
+                      <SubTitle>{scene.name} <Link href={`https://play.decentraland.org/?position=${scene.x},${scene.y}`}>({scene.x},{scene.y})</Link></SubTitle>
                       {hasRewards && rewardsItems.length === 0 && <div><Paragraph secondary>Sorry! This chest was empty</Paragraph></div>}
                     </div>
                     <div>
-                      {hasRewards && rewardsItems.length > 0 && <div>{rewardsItems.map(item => {
-                        return <div className={['reward', item.rarity || 'common'].join(' ')} title={item.tokens.title}>
+                      {hasRewards && rewardsItems.length > 0 && <div>{rewardsItems.map((item, i) => {
+                        return <div key={i} className={['reward', item.rarity || 'common'].join(' ')} title={item.tokens.title}>
                           <div className="name">{item.tokens.title}</div>
                           <div className="label">{item.rarity || 'common'}</div>
                           <img src={item.tokens.image} width={125} height={125} />
@@ -359,10 +427,10 @@ export default function IndexPage(props: any) {
                 </Grid.Column>
                 <Grid.Column mobile={3} >
                   <div>
-                    <Button href={`https://play.decentraland.org/?position=${scene.x}%2C${scene.y}`} primary inverted={!!reward} style={{ width: '100%' }} > {!reward ? 'Pending' : `Completed`} </Button>
+                    <Button href={`https://play.decentraland.org/?position=${scene.x},${scene.y}`} primary inverted={!!reward} style={{ width: '100%' }} > {!reward ? 'Incomplete' : `Completed`} </Button>
                   </div>
                   <div style={{ paddingTop: '1rem' }}>
-                    {hasRewardItems && <Button href={`https://play.decentraland.org/?position=${scene.x}%2C${scene.y}`} primary={hasPendingRewardItems} style={{ width: '100%' }} > {hasPendingRewardItems ? 'Claim' : 'Claimed'} </Button>}
+                    {hasRewardItems && <Button primary={hasPendingRewardItems} onClick={() => hasPendingRewardItems && reward && handleClaim(reward.contract, reward.transaction)} disabled={!hasPendingRewardItems} style={{ width: '100%' }} > {hasPendingRewardItems ? 'Claim' : 'Claimed'} </Button>}
                   </div>
                 </Grid.Column>
               </Grid.Row>
